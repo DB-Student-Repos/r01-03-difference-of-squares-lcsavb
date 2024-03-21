3619fc71b052855bb8401744e0372498df55df17
Lucas Barros <lcsavb@gmail.com>
2024-03-21 17:51:55 +0100

    substituted iteration by formula

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,10 @@
 pub fn square_of_sum(n: u32) -> u32 {
-    let mut sum = 0;
-    for i in 1..=n {
-        sum += i;
-    }
+    let sum = n * (n + 1) / 2;
     sum * sum
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
-    let mut sum = 0; // Initialize sum variable outside of the loop
-    for i in 1..=n {
-        let square = i * i;
-        sum += square; // Add square to sum
-    }
-    sum // Return sum at the end of the function
+    n * (n + 1) * (2 * n + 1) / 6
 }
 
 pub fn difference(n: u32) -> u32 {
